9ff166729b3580aa2c8fcf594e566be5acbdfb37
Alexandre JOURNET <alexandre.journet@gmail.com>
2024-10-21 22:44:51 +0200

    wip

--- Injections/PostgreSQLInjections.cs
+++ Injections/PostgreSQLInjections.cs
@@ -2,46 +2,45 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Solstice.Database.PostgreSQL.Injections
+namespace Solstice.Database.PostgreSQL.Injections;
+
+public static class PostgreSqlInjections
 {
-    public static class PostgreSqlInjections
+    public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, bool migrate = false)
+        where TDbContext : DbContext
     {
-        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, bool migrate = false)
-        where TDbContext : DbContext
+        services.AddDbContext<TDbContext>(options =>
         {
-            services.AddDbContext<TDbContext>(options =>
-            {
 #if DEBUG
-                options.EnableSensitiveDataLogging(true);
+            options.EnableSensitiveDataLogging(true);
 #endif
-                options.UseNpgsql(config);
-            }, ServiceLifetime.Transient);
+            options.UseNpgsql(config);
+        }, ServiceLifetime.Transient);
 
-            if (migrate)
-            {
-                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
-            }
+        if (migrate)
+        {
+            services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
         }
+    }
 
-        public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, ILoggerFactory? loggerFactory, bool migrate = false)
+    public static void AddDatabaseContext<TDbContext>(this IServiceCollection services, string config, ILoggerFactory? loggerFactory, bool migrate = false)
         where TDbContext : DbContext
+    {
+        services.AddDbContext<TDbContext>(options =>
         {
-            services.AddDbContext<TDbContext>(options =>
-            {
 #if DEBUG
-                options.EnableSensitiveDataLogging(true);
-                if (loggerFactory is not null)
-                {
-                    options.UseLoggerFactory(loggerFactory);
-                }
-#endif
-                options.UseNpgsql(config);
-            }, ServiceLifetime.Transient);
-
-            if (migrate)
+            options.EnableSensitiveDataLogging(true);
+            if (loggerFactory is not null)
             {
-                services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
+                options.UseLoggerFactory(loggerFactory);
             }
+#endif
+            options.UseNpgsql(config);
+        }, ServiceLifetime.Transient);
+
+        if (migrate)
+        {
+            services.BuildServiceProvider()!.GetService<TDbContext>()!.Database.Migrate();
         }
     }
 }
